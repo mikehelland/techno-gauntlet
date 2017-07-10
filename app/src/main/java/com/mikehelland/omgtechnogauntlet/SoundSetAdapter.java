6b63947e758e928845460ffe6bc513081ed7de04
mikehelland <mike@mikehelland.com>
2017-07-09 21:17:52 -0700

    soundadapter crlf issues

--- app/src/main/java/com/mikehelland/omgtechnogauntlet/SoundSetAdapter.java
+++ app/src/main/java/com/mikehelland/omgtechnogauntlet/SoundSetAdapter.java
@@ -12,29 +12,28 @@ public class SoundSetAdapter extends SimpleCursorAdapter {
     private final Context context;
     private int[] scale;
 
-    public SoundSetAdapter(Context context, int layout, Cursor c, String[] from, int[] to){
+    public SoundSetAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
         super(context, layout, c, from, to);
         this.context = context;
         this.scale = scale;
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent){
+    public View getView(final int position, View convertView, ViewGroup parent) {
         final View rowView;
         final ViewHolder holder;
-        if (convertView == null){
-            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             rowView = inflater.inflate(R.layout.saved_soundset_row, parent, false);
             holder = new ViewHolder();
 
-            holder.name = (TextView)rowView.findViewById(R.id.saved_data_tags);
+            holder.name = (TextView) rowView.findViewById(R.id.saved_data_tags);
             //holder.data = (TextView)rowView.findViewById(R.id.saved_data_date);
             rowView.setTag(holder);
 
-        }
-        else {
+        } else {
             rowView = convertView;
-            holder = (ViewHolder)convertView.getTag();
+            holder = (ViewHolder) convertView.getTag();
         }
 
         getCursor().moveToPosition(position);
